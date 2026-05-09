ad7f5d0d048b8bfd72f7503b8550370d853249a7
sanjaysanjust <42897195+sanjaysanjust@users.noreply.github.com>
2026-05-09 16:53:32 +0530

    Update SpringbootStarterWebApplication.java

--- src/main/java/com/example/demo/SpringbootStarterWebApplication.java
+++ src/main/java/com/example/demo/SpringbootStarterWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootStarterWebApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Inside App");
 		SpringApplication.run(SpringbootStarterWebApplication.class, args);
 	}
 
